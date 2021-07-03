5e3f72002f2832eb5e727f8647672769ef1ed023
HOD101s <manasbass.99@gmail.com>
2021-07-03 11:09:43 +0530

    modified: bug fixes

--- src/service/getShortLink.js
+++ src/service/getShortLink.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "http://127.0.0.1:5000";
 axios.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencoded";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
