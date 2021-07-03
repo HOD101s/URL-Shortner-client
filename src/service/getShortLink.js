6b2af43c3cd7149adb6d0fbbdcfb1a9704e211bb
HOD101s <manasbass.99@gmail.com>
2021-07-03 17:44:48 +0530

    modified: base url for local env

--- src/service/getShortLink.js
+++ src/service/getShortLink.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://127.0.0.1:5000";
+if (process.env.NODE_ENV != "production") {
+  axios.defaults.baseURL = "http://127.0.0.1:5000";
+}
 axios.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencoded";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
